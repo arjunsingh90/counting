67569d1b040fecd54e2724345a1f43016a35c817
arjunsingh90 <arjunchoukrayat5005@gmail.com>
2026-03-10 21:02:05 +0530

    add changes in style and js

--- script.js
+++ script.js
@@ -18,14 +18,38 @@ function addCount(num){
         count = MAX;
     }
 
-    localStorage.setItem("count", count);
+    save();
+}
 
+function removeCount(num){
+
+    count -= num;
+
+    if(count < 0){
+        count = 0;
+    }
+
+    save();
+}
+
+function save(){
+
+    localStorage.setItem("count", count);
     updateDisplay();
+
 }
 
 function updateDisplay(){
 
     document.getElementById("current").innerText =
         count.toLocaleString();
+
+    let percent = (count / MAX) * 100;
+
+    document.getElementById("percentage").innerText =
+        percent.toFixed(6) + "% completed";
+
+    document.getElementById("progress-bar").style.width =
+        percent + "%";
 
 }
